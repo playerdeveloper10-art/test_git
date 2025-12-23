4474f3e33642535d797129604f0aea7ddf027562
Player Developer <playerdeveloper10@gmail.com>
2025-12-23 12:01:40 -0300

    Meus principios fundamentais

--- ex001.py
+++ ex001.py
@@ -1,1 +1,24 @@
-print('Hello git vs code')
+msg = 'Hi'
+print(msg)
+# test
+# achar o que quiser, você é o principal
+# ninguem te controla, quem tenta fazer isso é um lixo de pessoa
+# eu vou pensar o que eu quiser
+# liberdade é um dos desejos fundamentais do ser humano
+# se tentarem te controlar lute de todas as formas
+# sempre que tentarem te controlar não deixe, lute
+# a pessoa que tenta fazer isso não é gente
+# se tentarem me controlar ou me reprimir eu vou reprimir de forma extrema
+# isso não é maldade
+# é autoproteção e uma luta primitiva por direitos fundamentais do ser humano 
+# seja o melhor para você mesmo, tenha a opinião que quiser
+# você pode fazer o que quiser desde de que ache certo ou seja o melhor para você
+# você pensa primeiro em você depois nos outros, isso é o extinto do ser humano
+# e consequentemente meu instinto
+# faça qualquer coisa que for possivel se tentarem te censurar
+# independente de quem for, porque a liberdade é um direito fundamental do ser humano
+# se tentarem te censurar você vai fazer o que for preciso
+# essa é a luta da minha vida a liberdade, criatividade, riqueza e poder de fazer mudanças reais
+# independente do que eu ouvir e pensar, eu sempre vou defender a liberdade do ser humano
+# a liberdade é sagrada, e não importa o que aconteça comigo eu sempre vou lutar e defender a liberdade
+# finalizando a principal luta da minha vida é a liberdade, e sempre será
